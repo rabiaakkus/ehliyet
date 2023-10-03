d777aee9d83e00f3c50ccd8d7a0d5fa38fbd640d
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2023-10-03 15:18:57 +0300

    Add files via upload

---
+++ BUTTON TEXTBOX/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUTTON_TEXTBOX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesaj_Click(object sender, EventArgs e)
+        {
+            int yas = 23;
+            
+            if (yas < 18)
+
+                MessageBox.Show("üzgünüm ehliyet yok");
+
+
+           
+
+            if(yas >= 18) {
+
+
+                MessageBox.Show("Ehliyet Alabilirsiniz");
+
+             }
+            
+        }  
+    }
+}
